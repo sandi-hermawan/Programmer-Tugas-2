2c942f01f2df33f9bb83660572175acfa06ea6b5
sandi-hermawan <sandi.tugas01@gmail.com>
2021-09-12 15:02:12 +0700

    Tugas Kedua

---
+++ Dilo Incremental Game/Assets/Control.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject particles;
+
+    private Vector2 mousePos;
+
+    void Start()
+    {
+        particles.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            particles.SetActive(true);
+            particles.transform.position = new Vector3(mousePos.x, mousePos.y, 0f);
+
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            particles.SetActive(false);
+        }
+    }
+}
---
+++ Script/SoundManagerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour
+{
+    public static AudioClip AchievementSound,CoinSound;
+    static AudioSource audioSrc;
+
+    void Start()
+    {
+        AchievementSound = Resources.Load<AudioClip>("achievement");
+        CoinSound = Resources.Load<AudioClip>("coinCollect");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public static void PlaySound(string clip)
+    {
+        switch (clip)
+        {
+            case "achievement":
+                audioSrc.PlayOneShot(AchievementSound);
+                break;
+            case "coinCollect":
+                audioSrc.PlayOneShot(CoinSound);
+                break;
+        }
+    }
+}
